0ab699448da186b2a43000bb62399657db6abfd3
Low-piched-walker <112291762+Low-piched-walker@users.noreply.github.com>
2023-03-17 09:08:35 +0800

    Add files via upload

---
+++ Assignment4/Clock/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace AlarmClock
+{
+    // 定义嘀嗒事件参数类
+    public class TickEventArgs : EventArgs
+    {
+        public DateTime Time { get; private set; }
+
+        public TickEventArgs(DateTime time)
+        {
+            Time = time;
+        }
+    }
+
+    // 定义响铃事件参数类
+    public class AlarmEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+
+        public AlarmEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+
+    // 定义闹钟类
+    public class AlarmClock
+    {
+        public event EventHandler<TickEventArgs> Tick;
+        public event EventHandler<AlarmEventArgs> Alarm;
+
+        private bool isAlarmSet = false;
+        private DateTime alarmTime;
+
+        // 设置闹钟时间
+        public void SetAlarmTime(DateTime time)
+        {
+            alarmTime = time;
+            isAlarmSet = true;
+        }
+
+        // 启动闹钟
+        public void Start()
+        {
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+
+                // 触发嘀嗒事件
+                Tick?.Invoke(this, new TickEventArgs(now));
+
+                if (isAlarmSet && now >= alarmTime)
+                {
+                    // 触发响铃事件
+                    Alarm?.Invoke(this, new AlarmEventArgs("Time's up!"));
+
+                    // 关闭闹钟
+                    isAlarmSet = false;
+                    break;
+                }
+
+                // 等待一秒钟
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+
+    // 测试闹钟类
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock alarmClock = new AlarmClock();
+
+            // 订阅嘀嗒事件
+            alarmClock.Tick += (sender, e) => Console.WriteLine("Tick: {0}", e.Time);
+
+            // 订阅响铃事件
+            alarmClock.Alarm += (sender, e) => Console.WriteLine("Alarm: {0}", e.Message);
+
+            // 设置闹钟时间为当前时间加30秒
+            alarmClock.SetAlarmTime(DateTime.Now.AddSeconds(30));
+
+            // 启动闹钟
+            alarmClock.Start();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment4/GenericList/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            // Print the list elements
+            Console.WriteLine("List elements:");
+            list.ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+
+            // Compute the maximum value
+            int max = list.Head.Data;
+            list.ForEach(i => { if (i > max) max = i; });
+            Console.WriteLine("Maximum value: " + max);
+
+            // Compute the minimum value
+            int min = list.Head.Data;
+            list.ForEach(i => { if (i < min) min = i; });
+            Console.WriteLine("Minimum value: " + min);
+
+            // Compute the sum of the elements
+            int sum = 0;
+            list.ForEach(i => sum += i);
+            Console.WriteLine("Sum of the elements: " + sum);
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public Node<T> Head
+        {
+            get { return head; }
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> node = head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
