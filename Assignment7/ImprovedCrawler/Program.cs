8266dad34a9b1bbadc6bfef2b438c02c0fedba49
Low-piched-walker <112291762+Low-piched-walker@users.noreply.github.com>
2023-04-13 20:57:06 +0800

    Add files via upload

---
+++ Assignment7/ImprovedCrawler/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImprovedCrawler
+{
+    class ImprovedCrawler
+    {
+        private HashSet<string> visited = new HashSet<string>();  // 已访问的链接
+        private Queue<string> unvisited = new Queue<string>();  // 待访问的链接
+        private string domain;  // 爬取的域名
+        private int count = 0;  // 记录已访问的页面数量
+
+        static void Main(string[] args)
+        {
+            ImprovedCrawler myCrawler = new ImprovedCrawler();
+            string startUrl = "http://www.cnblogs.com/dstang2000/";
+            if (args.Length >= 1) startUrl = args[0];
+            myCrawler.domain = GetDomain(startUrl);
+            myCrawler.unvisited.Enqueue(startUrl);
+            new Thread(myCrawler.Crawl).Start();
+        }
+
+        private void Crawl()
+        {
+            Console.WriteLine("开始爬行了.... ");
+            while (unvisited.Count > 0 && count < 10)
+            {
+                string current = unvisited.Dequeue();
+                Console.WriteLine("爬行" + current + "页面!");
+                string html = DownLoad(current);  // 下载
+                if (html == null) continue;
+                visited.Add(current);
+                count++;
+                if (IsHtmlFile(current))
+                {
+                    Parse(html, current);  // 解析,并加入新的链接
+                }
+                Console.WriteLine("爬行结束");
+            }
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private void Parse(string html, string current)
+        {
+            string strRef = @"(href|HREF)\s*=\s*[""'](?<url>[^""'#>]+)[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Groups["url"].Value.Trim();
+                if (strRef.Length == 0) continue;
+                string absoluteUrl = GetAbsoluteUrl(strRef, current);
+                if (absoluteUrl == null || visited.Contains(absoluteUrl)) continue;
+                if (IsSameDomain(absoluteUrl, domain))
+                {
+                    unvisited.Enqueue(absoluteUrl);
+                }
+            }
+        }
+
+        private static string GetDomain(string url)
+        {
+            Uri uri = new Uri(url);
+            return uri.Scheme + "://" + uri.Host;
+        }
+
+        private static bool IsSameDomain(string url1, string url2)
+        {
+            return GetDomain(url1) == GetDomain(url2);
+        }
+
+        private static bool IsHtmlFile(string url)
+        {
+            string extension = Path.GetExtension(url).ToLower();
+            return extension == ".htm" || extension == ".html" || extension == ".aspx"
+                || extension == ".php" || extension == ".jsp";
+        }
+
+        private static string GetAbsoluteUrl(string url, string current)
+        {
+            if (url.StartsWith("http")) return url;
+            if (url.StartsWith("//")) return "http:" + url;
+            if (url.StartsWith("/"))
+            {
+                Uri baseUri = new Uri(current);
+                return new Uri(baseUri, url).AbsoluteUri;
+            }
+            int lastSlashIndex = current.LastIndexOf("/");
+            string parentPath = current.Substring(0, lastSlashIndex + 1);
+
+            while (url.StartsWith("../"))
+            {
+                url = url.Substring(3);
+                int prevLastSlashIndex = parentPath.TrimEnd('/').LastIndexOf('/');
+                parentPath = parentPath.Substring(0, prevLastSlashIndex + 1);
+            }
+
+            return parentPath + url;
+        }
+    }
+}
---
+++ Assignment7/SimpleCrawler/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SimpleCrawler
+{
+    public partial class Form1 : Form
+    {
+        private SimpleCrawler crawler = new SimpleCrawler();
+        private Thread crawlerThread;
+        private bool isCrawling = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtUrl.Text = "http://www.cnblogs.com/dstang2000/";
+            crawler.OnPageDownloaded += Crawler_OnPageDownloaded;
+            crawler.OnError += Crawler_OnError;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!isCrawling)
+            {
+                lstUrls.Items.Clear();
+                lstErrors.Items.Clear();
+                crawlerThread = new Thread(crawler.Crawl);
+                crawlerThread.Start(txtUrl.Text.Trim());
+                isCrawling = true;
+                btnStart.Text = "停止";
+            }
+            else
+            {
+                crawlerThread.Abort();
+                isCrawling = false;
+                btnStart.Text = "开始";
+            }
+        }
+
+        private void Crawler_OnPageDownloaded(string url)
+        {
+            Action action = () => lstUrls.Items.Add(url);
+            if (InvokeRequired) Invoke(action);
+            else action();
+        }
+
+        private void Crawler_OnError(string url, string error)
+        {
+            Action action = () => lstErrors.Items.Add(url + " - " + error);
+            if (InvokeRequired) Invoke(action);
+            else action();
+        }
+    }
+
+    class SimpleCrawler
+    {
+        public event Action<string> OnPageDownloaded;
+        public event Action<string, string> OnError;
+
+        private HashSet<string> visited = new HashSet<string>();
+        private Queue<string> toCrawl = new Queue<string>();
+        private string startUrl;
+        private string host;
+
+        public void Crawl(object startUrlObj)
+        {
+            startUrl = (string)startUrlObj;
+            host = new Uri(startUrl).Host;
+            toCrawl.Enqueue(startUrl);
+            while (toCrawl.Count > 0)
+            {
+                string url = toCrawl.Dequeue();
+                try
+                {
+                    string html = DownLoad(url);
+                    OnPageDownloaded?.Invoke(url);
+                    visited.Add(url);
+                    Parse(html, url);
+                }
+                catch (Exception ex)
+                {
+                    OnError?.Invoke(url, ex.Message);
+                }
+            }
+        }
+
+        public string DownLoad(string url)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            string html = webClient.DownloadString(url);
+            string fileName = visited.Count.ToString();
+            File.WriteAllText(fileName, html, Encoding.UTF8);
+            return html;
+        }
+
+        private void Parse(string html, string currentUrl)
+        {
+            string strRef = @"(href|HREF)\s*=\s*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\'', '#', ' ', '>');
+                if (strRef.Length == 0) continue;
+                if (strRef.StartsWith("javascript:")) continue;
+                if (strRef.StartsWith("javascript")) continue;
+                string absoluteUrl = GetAbsoluteUrl(strRef, currentUrl);
+                if (absoluteUrl != null)
+                {
+                    if (!urls.ContainsKey(absoluteUrl))
+                    {
+                        urls.Add(absoluteUrl, false);
+                    }
+                }
+            }
+        }
+        private static string GetAbsoluteUrl(string url, string currentUrl)
+        {
+            if (url.StartsWith("http")) return url;
+            if (url.StartsWith("//")) return "http:" + url;
+            if (url.StartsWith("/"))
+            {
+                Uri uri = new Uri(currentUrl);
+                return uri.Scheme + "://" + uri.Host + url;
+            }
+            if (url.StartsWith("../"))
+            {
+                int idx = currentUrl.LastIndexOf('/');
+                string parentUrl = currentUrl.Substring(0, idx);
+                return GetAbsoluteUrl(url.Substring(3), parentUrl);
+            }
+            if (url.StartsWith("./"))
+            {
+                return GetAbsoluteUrl(url.Substring(2), currentUrl);
+            }
+            int end = currentUrl.LastIndexOf("/");
+            return currentUrl.Substring(0, end) + "/" + url;
+        }
+
+        private void Crawl()
+        {
+            while (urls.Count > 0 && count <= maxUrlCount)
+            {
+                string currentUrl = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    currentUrl = url;
+                }
+
+                if (currentUrl == null) continue;
+
+                try
+                {
+                    string html = DownLoad(currentUrl); // 下载
+                    urls[currentUrl] = true;
+                    count++;
+                    Parse(html, currentUrl);//解析,并加入新的链接
+                    UpdateUI(currentUrl, true);
+                }
+                catch (Exception ex)
+                {
+                    UpdateUI(currentUrl, false, ex.Message);
+                }
+            }
+        }
+
+        private string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void UpdateUI(string url, bool success, string errorMsg = null)
+        {
+            string result = url + (success ? "爬取成功" : "爬取失败");
+            if (!string.IsNullOrEmpty(errorMsg)) result += "，错误信息：" + errorMsg;
+            listBox1.Items.Add(result);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            urls.Clear();
+            count = 0;
+            maxUrlCount = (int)numericUpDown1.Value;
+            listBox1.Items.Clear();
+
+            string startUrl = textBox1.Text.Trim();
+            if (string.IsNullOrEmpty(startUrl))
+            {
+                MessageBox.Show("请输入起始URL");
+                return;
+            }
+
+            urls.Add(startUrl, false);//加入初始页面
+            new Thread(Crawl).Start();
+        }
+    }
+}
